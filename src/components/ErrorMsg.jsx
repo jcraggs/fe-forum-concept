5abfe9da91862cc7720fbc2f8ad7b657e7c85fda
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-27 22:48:31 +0000

    enabled handling of item loading errors

--- src/components/ErrorMsg.jsx
+++ src/components/ErrorMsg.jsx
@@ -5,6 +5,12 @@ class ErrorMsg extends React.Component {
   render() {
     return (
       <div className="errorMsgContainer">
+        {this.props.error.status === undefined && (
+          <div>
+            <p className="errorMsg">Server Error: item failed to load</p>
+            <p className="errorMsg">Status: 500</p>
+          </div>
+        )}
         {this.props.error.status === 500 && (
           <div>
             {" "}
@@ -14,12 +20,13 @@ class ErrorMsg extends React.Component {
             <p className="errorMsg">Status: {this.props.error.status}</p>
           </div>
         )}
-        {this.props.error.status !== 500 && (
-          <div>
-            <p className="errorMsg">{this.props.error.msg}</p>
-            <p className="errorMsg">Status: {this.props.error.status}</p>
-          </div>
-        )}
+        {this.props.error.status !== 500 &&
+          this.props.error.status !== undefined && (
+            <div>
+              <p className="errorMsg">{this.props.error.msg}</p>
+              <p className="errorMsg">Status: {this.props.error.status}</p>
+            </div>
+          )}
       </div>
     );
   }
