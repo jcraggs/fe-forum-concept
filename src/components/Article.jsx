f5c43e9da07ac434f461e3dc5b397e9ef6557d3c
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-27 22:44:37 +0000

    added comment loading error handling

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,14 +3,19 @@ import "../index.css";
 import FullArticleCard from "../components/FullArticleCard";
 import LogInSubTitle from "../components/LogInSubTitle";
 import CommentCards from "../components/CommentCards";
-// import CommentQuery from "../components/CommentQuery";
 import ErrorMsg from "../components/ErrorMsg";
 
 class Article extends React.Component {
   state = {
     sortBy: undefined,
     orderBy: undefined,
-    error: null
+    error: null,
+    commentError: null,
+    isLoading: true
+  };
+
+  setIsLoading = () => {
+    this.setState({ isLoading: false });
   };
 
   updateSortBy = param => {
@@ -25,13 +30,21 @@ class Article extends React.Component {
     this.setState({ error: err });
   };
 
+  commentError = err => {
+    this.setState({ commentError: err });
+  };
+
   render() {
     if (this.state.error !== null) return <ErrorMsg error={this.state.error} />;
+    if (this.state.commentError !== null) {
+      return <ErrorMsg error={this.state.commentError} />;
+    }
     return (
       <div className="ArticleAndCommentsContainer">
         <FullArticleCard
           urlInfo={this.props}
           triggerError={this.triggerError}
+          setIsLoading={this.setIsLoading}
         />
         {!this.props.user && <LogInSubTitle />}
         <CommentCards
@@ -39,9 +52,9 @@ class Article extends React.Component {
           user={this.props.user}
           sortByParam={this.state.sortBy}
           orderParam={this.state.orderBy}
-          triggerError={this.triggerError}
           updateSortBy={this.updateSortBy}
           updateOrder={this.updateOrder}
+          commentError={this.commentError}
         />
       </div>
     );
