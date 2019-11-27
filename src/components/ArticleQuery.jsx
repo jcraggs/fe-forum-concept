3d976592acebd9e7a0a966c05194041365f305b1
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-27 22:45:52 +0000

    refactored drop down select

--- src/components/ArticleQuery.jsx
+++ src/components/ArticleQuery.jsx
@@ -1,43 +1,65 @@
 import React from "react";
 import "../index.css";
 
-function ArticleQuery(props) {
-  return (
-    <div className="ArticleQuery">
-      <select
-        className="AQSelect"
-        onChange={event => {
-          props.updateSelectedTopic(event.target.value);
-        }}
-      >
-        <option value="">- Filter Topic -</option>
-        <option value="coding">Coding</option>
-        <option value="cooking"> Cooking</option>
-        <option value="football">Football</option>
-      </select>
-      <select
-        className="AQSelect"
-        onChange={event => {
-          props.updateSortBy(event.target.value);
-        }}
-      >
-        <option value="created_at">- Sort by - </option>
-        <option value="created_at">Date Posted</option>
-        <option value="comment_count">Number of comments</option>
-        <option value="votes">Number of votes</option>
-      </select>
-      <select
-        className="AQSelect"
-        onChange={event => {
-          props.updateOrder(event.target.value);
-        }}
-      >
-        <option value="desc">- Order by -</option>
-        <option value="desc">High > Low</option>
-        <option value="asc">Low > High</option>
-      </select>
-    </div>
-  );
+class ArticleQuery extends React.Component {
+  state = {
+    selectedTopic: "",
+    sortBy: "",
+    orderBy: ""
+  };
+  render() {
+    return (
+      <div className="ArticleQuery">
+        <select
+          aria-label="Filter articles by topic"
+          className="AQSelect"
+          value={this.state.selectedTopic}
+          onChange={event => {
+            this.setState({ selectedTopic: event.target.value });
+            this.props.updateSelectedTopic(event.target.value);
+          }}
+        >
+          <option value={""} disabled>
+            - Filter Topic -
+          </option>
+          <option value="coding">Coding</option>
+          <option value="cooking"> Cooking</option>
+          <option value="football">Football</option>
+        </select>
+        <select
+          aria-label="Sort articles by query"
+          className="AQSelect"
+          value={this.state.sortBy}
+          onChange={event => {
+            this.setState({ sortBy: event.target.value });
+            this.props.updateSortBy(event.target.value);
+          }}
+        >
+          <option value={""} disabled>
+            - Sort by -
+          </option>
+          <option value="created_at">Date Posted</option>
+          <option value="comment_count">Number of comments</option>
+          <option value="votes">Number of votes</option>
+        </select>
+        <select
+          aria-label="Order articles by query (high/low)"
+          className="AQSelect"
+          value={this.state.orderBy}
+          onChange={event => {
+            this.setState({ orderBy: event.target.value });
+            this.props.updateOrder(event.target.value);
+          }}
+        >
+          <option value={""} disabled>
+            - Order by -
+          </option>
+          <option value="desc">High > Low</option>
+          <option value="asc">Low > High</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default ArticleQuery;
