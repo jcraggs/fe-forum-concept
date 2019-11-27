6d6dfb72f9f8020037926cf3409bf44a4c9a1a27
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-27 22:47:39 +0000

    refactored drop down select

--- src/components/CommentQuery.jsx
+++ src/components/CommentQuery.jsx
@@ -1,30 +1,47 @@
 import React from "react";
 import "../index.css";
 
-function CommentQuery(props) {
-  return (
-    <div className="commentQuery">
-      <select
-        className="commentSelect"
-        onChange={event => {
-          props.updateSortBy(event.target.value);
-        }}
-      >
-        <option value="created_at">- Sort by - </option>
-        <option value="created_at">Date Posted</option>
-        <option value="votes">Number of votes</option>
-      </select>
-      <select
-        className="commentSelect"
-        onChange={event => {
-          props.updateOrder(event.target.value);
-        }}
-      >
-        <option value="desc">- Order by -</option>
-        <option value="desc">High > Low</option>
-        <option value="asc">Low > High</option>
-      </select>
-    </div>
-  );
+class CommentQuery extends React.Component {
+  state = {
+    sortBy: "",
+    orderBy: ""
+  };
+
+  render() {
+    return (
+      <div className="commentQuery">
+        <select
+          aria-label="Sort comments by query"
+          className="commentSelect"
+          value={this.state.sortBy}
+          onChange={event => {
+            this.setState({ sortBy: event.target.value });
+            this.props.updateSortBy(event.target.value);
+          }}
+        >
+          <option value={""} disabled>
+            - Sort by -
+          </option>
+          <option value="created_at">Date Posted</option>
+          <option value="votes">Number of votes</option>
+        </select>
+        <select
+          aria-label="Order comments by query (high/low)"
+          className="commentSelect"
+          value={this.state.orderBy}
+          onChange={event => {
+            this.setState({ orderBy: event.target.value });
+            this.props.updateOrder(event.target.value);
+          }}
+        >
+          <option value={""} disabled>
+            - Order by -
+          </option>
+          <option value="desc">High > Low</option>
+          <option value="asc">Low > High</option>
+        </select>
+      </div>
+    );
+  }
 }
 export default CommentQuery;
